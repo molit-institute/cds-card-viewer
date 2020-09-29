da25c95e6387356d7601d7f3176f4612c4777576
Katharina Zeller <Katharina.Zeller@molit.eu>
2020-09-29 15:28:47 +0200

    Added markup-support for suggestions and added type badge to actions

---
+++ src/utils/util.ts
@@ -0,0 +1,27 @@
+import DOMPurify from "dompurify";
+import marked from "marked";
+
+
+/**
+ * Creates safe HTML from markdown by using DOMPurify and marked.js.
+ *
+ * @param {String} markdown - the markdown text
+ * @param {boolean} [openLinksInNewTab=false] - whether to open generated links in new tab
+ * @returns {String} - the generated HTML
+ */
+export const markdownToHtml = (markdown, openLinksInNewTab = false) => {
+  if (!markdown || typeof markdown !== "string") {
+    return "";
+  }
+
+  let html = DOMPurify.sanitize(marked(markdown, { sanitize: true }));
+
+  if (openLinksInNewTab) {
+    html = html.replace(/<a /g, '<a target="_blank" rel="nofollow" ');
+  }
+  if (html.includes("&lt;/br&gt;") ) {
+    html = html.replace("&lt;/br&gt;", '</br>');
+  }
+
+  return html;
+};
